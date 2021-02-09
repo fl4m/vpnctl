11af2bd081760d5053fa769cda2b1a12d7bca6e4
fl4m <fl4m@users.noreply.github.com>
2021-02-09 12:12:00 +0100

    Add OVPN sub-package with list functionality.

---
+++ vpnctl/ovpn/configs.py
@@ -0,0 +1,19 @@
+import dbus
+import openvpn3
+
+
+class OpenVPNManager:
+    def __init__(self):
+        self.config_mgr = openvpn3.ConfigurationManager(dbus.SystemBus())
+
+    def list_configurations(self):
+        cfgs = self.config_mgr.FetchAvailableConfigs()
+        return [str(c.GetProperty('name')) for c in cfgs]
+
+
+def list_configuration_properties(config):
+    config = dbus.SystemBus().get_object('net.openvpn.v3.configuration',
+                                         config.GetPath())
+    prop_intf = dbus.Interface(config,
+                               dbus_interface="org.freedesktop.DBus.Properties")
+    return prop_intf.GetAll('net.openvpn.v3.configuration')
